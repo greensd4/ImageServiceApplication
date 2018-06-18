622f78bd16c4d2e3ed62efce38b06c9360586eba
daniel greenspan <greenspan1993@gmail.com>
2018-06-18 17:02:41 +0300

    image service

--- app/src/main/java/com/example/green/imageserviceapp/ImageService.java
+++ app/src/main/java/com/example/green/imageserviceapp/ImageService.java
@@ -1,16 +1,54 @@
 package com.example.green.imageserviceapp;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.List;
 
 public class ImageService extends Service {
+    BroadcastReceiver wifiReciever;
+    IntentFilter filter;
+    List<File> AndroidImages;
+
     public ImageService() {
+
     }
 
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        filter = new IntentFilter();
+        filter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
+        filter.addAction("android.net.wifi.STATE_CHANGE");
+        wifiReciever = new WifiReceiver();
+        // Here put the Code of Service
+        this.registerReceiver(this.wifiReciever, filter);
+    }
+
+    public int onStartCommand(Intent intent, int flag, int startId) {
+        Toast.makeText(this,"Service starting...", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
+    }
+
+    public void onDestroy() {
+        Toast.makeText(this,"Service ending...", Toast.LENGTH_SHORT).show();
+    }
+    public void startTransfer() {
+
+    }
+
+
 }
+
+
